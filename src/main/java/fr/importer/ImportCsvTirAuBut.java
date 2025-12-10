0fdf33a1046101c15766fa5c59d146b746c970dc
contant30 <romain.contant@hotmail.fr>
2025-12-10 09:48:25 +0100

    mise en place logger.info

--- src/main/java/fr/importer/ImportCsvTirAuBut.java
+++ src/main/java/fr/importer/ImportCsvTirAuBut.java
@@ -10,11 +10,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Classe qui permet d'importer les séances de tires buts
  * depuis un fichier CSV shootouts
  */
 public class ImportCsvTirAuBut {
+    private static final Logger logger = LoggerFactory.getLogger(ImportCsvBut.class);
+
 
     public static void main(String[] args) {
 
@@ -64,8 +69,8 @@ public class ImportCsvTirAuBut {
                 long dureeBatchMs = finBatch - debut500;
                 double dureeBatchSec = dureeBatchMs / 1000.0;
 
-                System.out.println("Lignes traitées : " + nbSeanceTirAuBut+ " | temps pour les 500 dernières : "
-                        + dureeBatchSec + " s");
+                logger.info("Lignes traitées : {} | temps pour les 500 dernières : {} s",
+                nbSeanceTirAuBut, dureeBatchSec );
 
                 // relance le temps pour le prochain bloc de 500
                 debut500 = finBatch;
@@ -82,8 +87,8 @@ public class ImportCsvTirAuBut {
             long dureeMs = fin - debut;
             double dureeSec = dureeMs / 1000.0;
 
-            System.out.println("Nombre de séance importés : " + nbSeanceTirAuBut);
-            System.out.println("Temps d'import : " + dureeMs + " ms (" + dureeSec + " s)");
+            logger.info("Nombre de séance importés : {}" , nbSeanceTirAuBut);
+            logger.info("Temps d'import : {} ms {} s" , dureeMs ,dureeSec );
 
         } catch (Exception e){
             // Si une erreur, un rollback est effectué
